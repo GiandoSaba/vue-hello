4acf69f1830253825f0d4a7e497a0fa7e1d957ce
GiandoSaba <giandosaba997@gmail.com>
2021-12-15 23:24:44 +0100

    end_without

---
+++ bonus/script.js
@@ -0,0 +1,12 @@
+//     Bonus:
+// In una cartella bonus:
+// Inseriamo un input per visualizzare in pagina un testo inserito da un utente.
+// Inseriamo una immagine e accanto un numero che identifica il numero di prodotti desiderati, i dati devono venire da Vue.
+// Poi aggiungiamo un pulsante per aumentare questo numero.
+
+const app = new Vue({
+    el: '#app',
+    data: {
+       
+    }
+});
